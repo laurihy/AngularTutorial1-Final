2b7837992b38bbc85d18d8316ddf7100ef3dd2d5
colevscode <cole@Coles-MacBook-Pro.local>
2013-03-09 19:42:15 -0800

    updates to simplify changes between my version of the completed angular tutorial and the final

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,31 +1,30 @@
 function ListController($scope){
 
-	$scope.items = [
-	    {text:'Hello', deleted: false, archived: false}
-	];
+    $scope.items = [
+        {text:'Hello', deleted: false, archived:false}
+    ];
 
-	$scope.addNewItem = function(){
-	    newItem = {
-	        text: $scope.itemText,
-	        deleted: false,
-	        archived: false
-	    }
-	    $scope.items.push(newItem);
+    $scope.addNewItem = function(){
+        newItem = {
+            text: $scope.itemText,
+            deleted: false,
+            archived: false
+        }
+        $scope.items.push(newItem);
 
-	    // clear the form text, 2-way binding \o/
-	    $scope.itemText = '';
-	}
+        // clear the form text, 2-way binding \o/
+        $scope.itemText = '';
+    }
 
-	$scope.deleteItem = function(item){
-	    item.deleted = true;
-	}
+    $scope.deleteItem = function(item){
+        item.deleted = true;
+    }
 
-	$scope.archiveItems = function(){
-		for(i in $scope.items){
-			if($scope.items[i].deleted){
-				$scope.items[i].archived = true;
-			}
-		}
-	}
-
+    $scope.archiveItems = function(){
+    	for (var i in $scope.items) {
+    		if ($scope.items[i].deleted) {
+    			$scope.items[i].archived = true;
+    		}
+    	}
+    }
 }
